d18be1bb20b3326614701f9f7084d9a20d237771
praktisa <115921411+praktisa@users.noreply.github.com>
2023-11-07 16:45:25 +0000

    menambahkan admin page

--- app/App/KendaraanDinas/@Admin/page.tsx
+++ app/App/KendaraanDinas/@Admin/page.tsx
@@ -2,10 +2,10 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function page() {
+export default function AdminPage() {
     return (
         <>
-
+            <div>ini adalah Admin Pagi</div>
         </>
     )
 }
